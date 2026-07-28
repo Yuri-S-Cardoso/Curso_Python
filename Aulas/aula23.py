93c8b4577f2c0ed19ba438d93dc402cdf0ffdbd7
Yuri-S-Cardoso <yuridscardoso@gmail.com>
2026-07-28 11:40:16 -0300

    inclusão de aulas

---
+++ Aulas/aula23.py
@@ -0,0 +1,20 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+# senha = input('Senha: ')
+
+# if senha == '12345':
+#     print('Entrou')
+# else:
+#     print('Senha incorreta')
+
+# if senha != '12345':
+#     print('Senha incorreta')
+
+# if not senha:
+#     print('Você não digitou nada')
+
+print(not True) or (not 0) # False
+print(not False) or (not 1)# True

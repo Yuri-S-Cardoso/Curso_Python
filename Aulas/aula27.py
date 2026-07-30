8ee4b93b24b5fe1fde89c15584d43117d3494a08
Yuri-S-Cardoso <yuridscardoso@gmail.com>
2026-07-29 23:35:08 -0300

    Atualização aulas

---
+++ Aulas/aula27.py
@@ -0,0 +1,23 @@
+# Fatiamento de strings
+# 012345678
+# Olá Mundo
+# -987654321
+# Fatiameto [i:f:p] [::]
+# Obs.: a funçã len retorna a quantidade de caracteres da str
+
+variavel = 'Olá mundo'
+
+print(variavel[4])
+print(variavel[-5])
+print(variavel[4:])
+print(variavel[4:7])
+print(variavel[:5])
+print(variavel[-8:-2])
+print(len(variavel))
+print(len(variavel[3]))
+print(variavel[0:len(variavel):1])
+print(variavel[0:9:1])
+print(variavel[0:9:2])
+print(variavel[::-1])
+print(variavel[-1:-10:-1])
+print(variavel[-1:-10:-2])
---
+++ Aulas/aula29.py
@@ -0,0 +1,4 @@
+# Introdução a try/except
+# try -> Tentar executar o código
+# except -> ocorreu algum erro ao tentar executar
+
